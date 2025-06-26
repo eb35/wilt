69271e354e13fb1f56cf590b2cbcbd44002caa97
Rob Henry <roberthenry82@hotmail.com>
2025-06-26 01:15:20 -0500

    Disabling keystatic in production

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,13 @@ import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
 
+const isBuild = process.env.NODE_ENV === 'production';
+
 // https://astro.build/config
 export default defineConfig({
   adapter: netlify(),
-  integrations: [react(), markdoc(), keystatic()]
+  integrations: [
+    react(), 
+    markdoc(), 
+    (isBuild ? [] : [keystatic()]),]
 });
