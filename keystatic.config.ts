ca020bcb1852f19b63b0c1ac80514a60ace8aa02
Rob Henry <roberthenry82@hotmail.com>
2025-06-27 00:52:16 -0500

    KeyStatic tweaks
    Adding settings singleton and pages collection. Also moving to data folder.

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,15 +1,45 @@
-import { config, fields, collection } from '@keystatic/core';
+import { config, fields, singleton, collection } from '@keystatic/core';
 
 export default config({
   storage: {
     kind: 'local',
   },
+  singletons: {
+    settings: singleton({
+      label: 'Settings',
+      path: 'src/data/settings',
+      format: { data: "yaml" },
+      schema: {
+        siteTitle: fields.text({
+          label: 'Site Title',
+          description: 'The title of your site, displayed in the header and browser tab.',
+        }),
+        siteDescription: fields.text({
+          label: 'Site Description',
+          description: 'A brief description of your site, used for SEO and social sharing.',
+        }),
+      }
+    }),
+  },
 
   collections: {
+    pages: collection({
+      label: 'Pages',
+      slugField: 'title',
+      path: 'src/data/pages/*',
+      format: { contentField: 'content' },
+      schema: {
+        title: fields.slug({ name: { label: 'Title' } }),
+        content: fields.markdoc({
+          label: 'Content',
+          description: 'The main content of the page, written in Markdoc format.',
+        })
+      }
+    }),
     posts: collection({
       label: 'Posts',
       slugField: 'title',
-      path: 'src/content/posts/*',
+      path: 'src/data/posts/*',
       format: { contentField: 'content' },
       schema: {
         title: fields.slug({ name: { label: 'Title' } }),
--- src/content.config.ts
+++ src/content.config.ts
@@ -2,11 +2,13 @@ import { defineCollection, reference, z } from "astro:content";
 import { glob } from "astro/loaders";
 
 const posts = defineCollection({
-    loader: glob({ pattern: "**/[^_]*.mdoc", base: "./src/content/posts" }),
+    loader: glob({ pattern: "**/[^_]*.mdoc", base: "./src/data/posts" }),
     schema: z.object({
         title: z.string(),
         description: z.string().optional(),
     }),
 });
 
-export const collections = { posts };
+export const collections = { 
+    posts
+};
